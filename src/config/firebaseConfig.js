ee635e7e258cfbe3c277d4f057b65dd143c2562a
YubaNeupane <neopaneyb2@gmail.com>
2020-07-07 15:48:10 -0400

    first commit

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,13 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9HOLCAafwJH6rZXDKiQGMcNeJp8DblTI",
-  authDomain: "inventory-system-55e4b.firebaseapp.com",
-  databaseURL: "https://inventory-system-55e4b.firebaseio.com",
-  projectId: "inventory-system-55e4b",
-  storageBucket: "inventory-system-55e4b.appspot.com",
-  messagingSenderId: "536330619575",
-  appId: "1:536330619575:web:c2dd552bafb12a0ca31b32"
+
 };
 
 firebase.initializeApp(firebaseConfig);
